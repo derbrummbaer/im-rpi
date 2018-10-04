0e24ca041125409dbc5f12e9280a345191af490e
Sven Berger <sven@berger-lembeck.de>
2018-10-04 13:35:16 +0200

    IOBroker: Start Scripte um Kopieren erweitert

--- iobroker/scripte/run.sh
+++ iobroker/scripte/run.sh
@@ -1,12 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 if  [ "$(ls -A /opt/iobroker)" ]; then
+    echo "Neu Installation, Verschiebe Daten"
     mv /opt/iobroker.data /opt/iobroker
     cd /opt/iobroker
 else
+    echo "Installation schon vorhanden"
     cd /opt/iobroker
 fi
 
+echo "Konfiguriere IOBroker"
 ./iobroker host $(cat .install_host) && echo $(hostname) > .install_host
 
+echo "Starte IOBroker"
 node node_modules/iobroker.js-controller/controller.js
